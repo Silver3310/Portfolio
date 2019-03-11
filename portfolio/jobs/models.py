5cd12ff5ae065d964856451b1a7ea2f7d5b6c6c7
Silver3310 <silver3310@github.com>
2019-03-11 10:57:44 +0700

    Add a model for jobs

--- portfolio/jobs/models.py
+++ portfolio/jobs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Job(models.Model):
+    """Custom model for jobs
+    Attributes:
+        images (image): images for jobs
+        summary (text): summaries for jobs
+    """
+    image = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=200)
